c2b386f317609e067d1eadd86e6fae479aea0e87
Sombrero1 <vovashevchenko1964@mail.ru>
2021-08-26 17:35:37 +0300

    tags

--- src/main/java/com/mts/lts/dao/TagRepository.java
+++ src/main/java/com/mts/lts/dao/TagRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
     Tag findByName(String tag);
+    Boolean existsByName(String name);
 }
--- src/main/java/com/mts/lts/service/NewsListerService.java
+++ src/main/java/com/mts/lts/service/NewsListerService.java
@@ -1,25 +1,32 @@
 package com.mts.lts.service;
 
 import com.mts.lts.dao.NewsRepository;
+import com.mts.lts.dao.TagRepository;
 import com.mts.lts.dao.UserRepository;
 import com.mts.lts.domain.News;
+import com.mts.lts.domain.Tag;
 import com.mts.lts.service.exceptions.NewsNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class NewsListerService {
     private final NewsRepository newsRepository;
+    private final TagRepository tagRepository;
     private final UserRepository userRepository;
 
     @Autowired
-    public NewsListerService(NewsRepository newsRepository, UserRepository userRepository) {
+    public NewsListerService(NewsRepository newsRepository, TagRepository tagRepository, UserRepository userRepository) {
         this.newsRepository = newsRepository;
+        this.tagRepository = tagRepository;
         this.userRepository = userRepository;
     }
 
@@ -41,9 +48,19 @@ public class NewsListerService {
             news.setCreatedBy(userRepository.findUserByEmail(authentication.getName()).orElseThrow(NoSuchElementException::new));
             news.setCreatedAt(LocalDateTime.now());
         }
-
         news.setUpdatedBy(userRepository.findUserByEmail(authentication.getName()).orElseThrow(NoSuchElementException::new));
         news.setUpdatedAt(LocalDateTime.now());
+
+        for (Tag tag:news.getTags()) {
+
+            if(!tagRepository.existsByName(tag.getName())) tagRepository.save(tag);
+            Tag tg = tagRepository.findByName(tag.getName());
+
+            if (tg.getNews() == null) tg.setNews(new HashSet<News>());
+            tg.getNews().add(news);
+
+        }
+
         newsRepository.save(news);
     }
 
