80e5a019dda8b98fcc04bbb54e9f5c22d01ab208
Sombrero1 <vovashevchenko1964@mail.ru>
2021-08-26 14:53:45 +0300

    news update: author and date update

--- src/main/java/com/mts/lts/service/NewsListerService.java
+++ src/main/java/com/mts/lts/service/NewsListerService.java
@@ -1,20 +1,26 @@
 package com.mts.lts.service;
 
 import com.mts.lts.dao.NewsRepository;
+import com.mts.lts.dao.UserRepository;
 import com.mts.lts.domain.News;
 import com.mts.lts.service.exceptions.NewsNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Component
 public class NewsListerService {
     private final NewsRepository newsRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public NewsListerService(NewsRepository newsRepository) {
+    public NewsListerService(NewsRepository newsRepository, UserRepository userRepository) {
         this.newsRepository = newsRepository;
+        this.userRepository = userRepository;
     }
 
     public List<News> findAll() {
@@ -29,8 +35,18 @@ public class NewsListerService {
         return newsRepository.getOne(id);
     }
 
-    public void save(News news) {
+    public void save(News news, Authentication authentication)
+    {
+        if(news.getId() == null){
+            news.setCreatedBy(userRepository.findUserByEmail(authentication.getName()).orElseThrow(NoSuchElementException::new));
+            news.setCreatedAt(LocalDateTime.now());
+        }
+
+        news.setUpdatedBy(userRepository.findUserByEmail(authentication.getName()).orElseThrow(NoSuchElementException::new));
+        news.setUpdatedAt(LocalDateTime.now());
         newsRepository.save(news);
     }
+
+
 
 }
